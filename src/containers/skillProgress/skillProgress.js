03cfab4c97e5e693357ddd4c40751eb7c14396d1
João Pedro Lage <JoaoPedro.Lage@outlook.com>
2025-08-26 21:32:08 -0300

    style: add multilingual support for skill progress section with English and Portuguese translations

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Progress.scss";
-import {illustration, techStack} from "../../portfolio";
-import {Fade} from "react-reveal";
+import { illustration, techStack } from "../../portfolio";
+import { Fade } from "react-reveal";
 import Build from "../../assets/lottie/build";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import { PortfolioContext } from "../../contexts/PortfolioContext";
 
 export default function StackProgress() {
+  const { isEnglish } = useContext(PortfolioContext);
+
   if (techStack.viewSkillBars) {
     return (
       <Fade bottom duration={1000} distance="20px">
         <div className="skills-container">
           <div className="skills-bar">
-            <h1 className="skills-heading">Proficiency</h1>
+            <h1 className="skills-heading">{isEnglish ? 'Proficiency' : 'Proficiência'}</h1>
             {techStack.experience.map((exp, i) => {
               const progressStyle = {
                 width: exp.progressPercentage
               };
               return (
                 <div key={i} className="skill">
-                  <p>{exp.Stack}</p>
+                  <p>{exp.Stack === 'Programming' && !isEnglish ? 'Programação' : exp.Stack}</p>
                   <div className="meter">
                     <span style={progressStyle}></span>
                   </div>
