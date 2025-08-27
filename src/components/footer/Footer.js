90d5f75bd72094925d17339790d136a78c381520
João Pedro Lage <JoaoPedro.Lage@outlook.com>
2025-08-26 21:31:43 -0300

    style: add multilingual support for footer text in English and Portuguese

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,20 +3,23 @@ import "./Footer.scss";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
+import {PortfolioContext} from "../../contexts/PortfolioContext";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
+  const {isEnglish} = useContext(PortfolioContext);
+
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Me with help of ")}
+          {emoji(isEnglish ? "Made with ❤️ by Me with help of " : "Feito com ❤️ por mim com ajuda do ")}
           <a
             href="https://github.com/saadpasta/developerFolio"
             target="_blank"
             rel="noreferrer"
           >
-            DeveloperFolio Team
+            {isEnglish ? "DeveloperFolio Team" : "Time DeveloperFolio"}
           </a>
         </p>
       </div>
